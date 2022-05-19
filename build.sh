ebb84bbb6160b38244af7773760e4c93c97357e7
Diogo Oliveira de Melo <dmelo87@gmail.com>
2022-05-19 17:28:48 -0300

    Try and use ios path

--- build.sh
+++ build.sh
@@ -1,7 +1,8 @@
 #!/bin/bash -e
 
 script_path=$(cd $(dirname ${0}); pwd)
-cp -r ${script_path}/fastlane ./
+cd ios
+cp -r ${script_path}/fastlane ./fastlane/
 pwd
-bundle config path vendor/bundle
+bundle config path vendor/bundle/
 bundle exec fastlane export_ipa
