8e2d9ad62ab5ca1be862559d706a8deb0e1713a2
Diogo Oliveira de Melo <dmelo87@gmail.com>
2022-05-19 16:59:04 -0300

    Run using bundle exec

--- build.sh
+++ build.sh
@@ -1,13 +1,5 @@
 #!/bin/bash -e
 
-if ! type fastlane > /dev/null 2>&1; then
-  if type brew > /dev/null 2>&1; then
-    brew install fastlane
-  else
-    sudo gem install fastlane -NV
-  fi
-fi
-
 script_path=$(cd $(dirname ${0}); pwd)
 cp -r ${script_path}/fastlane ./
-fastlane export_ipa
+bundle exec fastlane export_ipa
